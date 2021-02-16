7fc4cf6c7260b7642360568460f6a89ded1c0949
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-02-16 06:12:41 +0000

    Initial commit. This is a counter, the program has to find the even numbers from 4 to 20.
    Practice which statement.

---
+++ src/com/gmail/norbert/sapi/Main.java
@@ -0,0 +1,35 @@
+package com.gmail.norbert.sapi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    // this is a while exercise
+    //System.out.println(Motor.isEven(3));
+
+            int sum = 0;
+            int countStart = 1;
+            int last = 20;
+
+            while (x < y) {
+                System.out.println(count);
+                count++;
+                sum += 1;
+                System.out.println("flow: " + count);
+                System.out.println("sum: " + sum);
+            }
+            return number % 2 == 0;
+        }
+
+        boolean checkNum = Motor.isEven(num);
+        System.out.println(checkNum);
+
+    }
+
+    public static boolean isEven(int number) {
+    if (number % 2 == 0){
+        return true;
+    } else {
+        return false;
+    }
+}
---
+++ src/com/gmail/norbert/sapi/Motor.java
@@ -0,0 +1,5 @@
+package com.gmail.norbert.sapi;
+
+public class Motor {
+
+}
