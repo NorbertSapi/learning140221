9f594aab1bf3711782b052fa7f910ea60d82c5bd
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-02-16 07:04:52 +0000

    counter - Up & Running

--- src/com/gmail/norbert/sapi/Main.java
+++ src/com/gmail/norbert/sapi/Main.java
@@ -1,22 +1,23 @@
 package com.gmail.norbert.sapi;
 
 public class Main {
-
     public static void main(String[] args) {
-        // write your code here
-        // this is a while exercise
-        //System.out.println(Motor.isEven(3));
-
         // initialising the variables
-        int sum = 0;
         int countStart = 4;
         int counterEnd = 20;
 
         // while loop - counter
         while (countStart < counterEnd) {
             countStart++;
+            if(!isEven(countStart)) {
+                continue;
+            }
             System.out.println("flow: " + countStart);
         }
         System.out.println("last number " + countStart);
+    }
+
+    private static boolean isEven(int num) {
+        return num % 2 == 0;
     }
 }
